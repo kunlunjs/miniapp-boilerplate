97375d09dbfcb6d2d5c7b4bc675776c5e05633cc
erguotou <erguotou525@gmail.com>
2021-03-22 18:51:04 +0800

    chore: 🔧 change babel config file

--- .babelrc.js
+++ .babelrc.js
@@ -1,9 +1,9 @@
-// babel-preset-taro 更多选项和默认值：
-// https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
   presets: [
     [
+      // babel-preset-taro 更多选项和默认值：
+      // https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
       'taro',
       {
         framework: 'react',

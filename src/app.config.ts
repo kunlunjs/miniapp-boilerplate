c9c0cdda8515b6b5cf67a746df8f40e1d36b9b11
erguotou <erguotou525@gmail.com>
2021-03-31 15:08:47 +0800

    chore: 🔧 t1

--- src/app.config.ts
+++ src/app.config.ts
@@ -19,7 +19,7 @@ const config: MinaConfig = {
         iconPath: 'assets/tabbar/home.png',
         selectedIconPath: 'assets/tabbar/home_on.png',
         pagePath: 'pages/index/index',
-        text: '首页',
+        text: '首页1',
       },
       {
         iconPath: 'assets/tabbar/parlor.png',
